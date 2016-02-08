ec222391df391488fec6feab7fc1466c9111b14e
amuehlbauer <andreas.muehlbauer@ianus-it.de>
2016-02-08 17:14:56 +0100

    Update deploy.sh

--- files/deploy.sh
+++ files/deploy.sh
@@ -6,8 +6,7 @@ export VERSION="0.9.8"
 export DESTINATION="/opt/guacamole"
 mkdir -p "$DESTINATION"
 echo "Downloading JDBC auth version $VERSION ..."
-curl -L
-"http://sourceforge.net/projects/guacamole/files/current/extensions/guacamole-auth-jdbc-$VERSION.tar.gz/download" | \
+curl -L "http://sourceforge.net/projects/guacamole/files/current/extensions/guacamole-auth-jdbc-$VERSION.tar.gz/download" | \
 tar -zx \
  -C "$DESTINATION" \
  --wildcards \
@@ -17,12 +16,10 @@ tar -zx \
  "*.sql"
 export DESTINATION="/usr/local/tomcat/webapps"
 echo "Downloading Guacamole version $VERSION to $DESTINATION ..."
-curl -L "http://sourceforge.net/projects/guacamole/files/current/binary/guacamole-${VERSION}.war/download" >
-"$DESTINATION/ROOT.war"
+curl -L "http://sourceforge.net/projects/guacamole/files/current/binary/guacamole-${VERSION}.war/download" > "$DESTINATION/ROOT.war"
 
 export DESTINATION="/opt/guacamole"
 echo "Downloading PostgreSQL JDBC driver ..."
 mkdir -p "$DESTINATION/postgresql/"
-curl -L "https://jdbc.postgresql.org/download/postgresql-9.4-1201.jdbc41.jar" >
-"$DESTINATION/postgresql/postgresql-9.4-1201.jdbc41.jar"
+curl -L "https://jdbc.postgresql.org/download/postgresql-9.4-1201.jdbc41.jar" > "$DESTINATION/postgresql/postgresql-9.4-1201.jdbc41.jar"
 apt-get update && apt-get install -y postgresql-client
